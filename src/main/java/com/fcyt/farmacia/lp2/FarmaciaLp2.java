27fe8fff034959b199f44354bcba3412e5eb0b26
cmendieta2311@gmail.com <cmendieta2311@gmail.com>
2025-10-27 21:35:34 -0300

    se agrega Categoria

--- src/main/java/com/fcyt/farmacia/lp2/FarmaciaLp2.java
+++ src/main/java/com/fcyt/farmacia/lp2/FarmaciaLp2.java
@@ -5,10 +5,15 @@ package com.fcyt.farmacia.lp2;
 
 import com.fcyt.farmacia.lp2.controlador.CategoriaController;
 import com.fcyt.farmacia.lp2.controlador.RolController;
+import com.fcyt.farmacia.lp2.controlador.UnidadMedidaController;
 import com.fcyt.farmacia.lp2.modelo.dao.CategoriaDaoImpl;
 import com.fcyt.farmacia.lp2.modelo.dao.RolDaoImpl;
+import com.fcyt.farmacia.lp2.modelo.dao.UnidadMedidaDaoImpl;
+import com.fcyt.farmacia.lp2.modelo.entidad.Producto;
 import com.fcyt.farmacia.lp2.vista.GUICategoria;
 import com.fcyt.farmacia.lp2.vista.GUIRol;
+import com.fcyt.farmacia.lp2.vista.GUIUnidadMedida;
+import com.fcyt.farmacia.lp2.vista.VentanaPrincipal;
 
 /**
  *
@@ -17,13 +22,9 @@ import com.fcyt.farmacia.lp2.vista.GUIRol;
 public class FarmaciaLp2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-//        RolDaoImpl rolDao = new RolDaoImpl();
-//        GUIRol gui = new GUIRol(null, true);
-//        RolController ctrl = new RolController(gui, rolDao);
-        CategoriaDaoImpl rolDao = new CategoriaDaoImpl();
-        GUICategoria gui = new GUICategoria(null, true);
-        CategoriaController ctrl = new CategoriaController(gui, rolDao);
+        VentanaPrincipal main = new VentanaPrincipal();
+        main.setVisible(true);
+        
 
     }
 }
